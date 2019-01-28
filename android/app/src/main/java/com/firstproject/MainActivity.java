d92fd2ea5f306887741b430518e03ac11218c585
SKolevatov <sergei.kolevatov@nytimes.com>
2019-01-28 11:47:32 -0500

    7th task: launch screen implemented

--- android/app/src/main/java/com/firstproject/MainActivity.java
+++ android/app/src/main/java/com/firstproject/MainActivity.java
@@ -1,6 +1,7 @@
 package com.firstproject;
-
+import android.os.Bundle; // here
 import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +12,10 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "FirstProject";
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);  // here
+        super.onCreate(savedInstanceState);
     }
 }
